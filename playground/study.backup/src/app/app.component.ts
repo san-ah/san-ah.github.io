3570c4623ed9fc20e37058b8ae5c030029884873
박산아 <sanah.dev@gmail.com>
2021-05-21 12:24:19 +0900

    <feat> 0514 ngOnChanges 변하는 데이터 감지하기

--- playground/study.backup/src/app/app.component.ts
+++ playground/study.backup/src/app/app.component.ts
@@ -10,17 +10,16 @@ export class AppComponent {
   title = 'study0514';
   item: string;
   items = [];
-
   
   // 리스트에 입력한 정보 추가
   onclickAdd(item: string) {
     this.item = item;  // onclickAdd의 item은 AppComponent의 item이다??
     this.items.push(item);
-    // console.log(this.items);
+    console.log(this.items);
+    this.items = [...this.items, item]
+    return ;
     // 배열의 마지막값
     // console.log(this.items.slice(-1)[0]);
-    return this.items;
   }
-
 
 }
--- playground/study.backup/src/app/paragraph/paragraph.component.ts
+++ playground/study.backup/src/app/paragraph/paragraph.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component,  EventEmitter,  Input, OnInit, Output, SimpleChanges,  } from '@angular/core';
 
 @Component({
   selector: 'app-paragraph',
@@ -6,14 +6,31 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./paragraph.component.sass']
 })
 export class ParagraphComponent implements OnInit {
-
+  
+  
+  //-- 라이프사이클 
+  //-- https://abelog.netlify.app/Angular/%EC%95%B5%EA%B7%A4%EB%9F%AC-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8-%EB%9D%BC%EC%9D%B4%ED%94%84%EC%82%AC%EC%9D%B4%ED%81%B4-(angular-component-lifecycle)
+  //-- https://js2prince.tistory.com/entry/Angular-Component-LifeCycle
+  
+  // 타입스크립트
   constructor() { }
+  
+  // 앵귤러 ngOnChanges ngOnInit
+  // @Input로 호출
+ 
+  // @Input() item: string;
+  @Input() items: Array <String>;
+  // 왜...?? <input _ngcontent-pqn-c41 type=​"text" ng-model=​"item">​
 
-  ngOnInit(): void {
-    
+  ngOnChanges(changes: SimpleChanges) {
+    // 작성시 참고 https://www.python2.net/questions-1153319.htm
+    console.log("ngOnChanges:",this.items);
+    console.log(changes.items.currentValue);
   }
 
-  @Input() item: string;
-  @Input() items: string[];
+  ngOnInit(): void {
+    console.log("ngOnInit:",this.items);
+  }
 
+  // 업데이트 할 때마다 어레이를 복사해야한다??https://stackoverflow.com/questions/43223582/why-angular-2-ngonchanges-not-responding-to-input-array-push
 }
